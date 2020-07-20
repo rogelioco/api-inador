c8ff9c8ef68596a70318cf7758316ef074bbb134
myusername <183464@ids.upchiapas.edu.mx>
2020-07-20 18:43:12 -0500

    cors allow headers

--- apiTemp/settings.py
+++ apiTemp/settings.py
@@ -59,14 +59,15 @@ MIDDLEWARE = [
 ]
 
 #CORS
-#CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
-CORS_ORIGIN_WHITELIST = [
+#CORS_ORIGIN_WHITELIST = [
     #"http://inador-console.ddns.net/",
-    "http://100.25.154.25",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000"
-]
+    #"http://100.25.154.25",
+    #"http://localhost:8000",
+    #"http://127.0.0.1:8000"
+#]
 
 CORS_ALLOW_METHODS = [
     'DELETE',
@@ -75,6 +76,18 @@ CORS_ALLOW_METHODS = [
     'PATCH',
     'POST',
     'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
 #CORS_ALLOW_CREDENTIALS = True
 

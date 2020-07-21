d59956315ed516d9be89ca20387470ffc99c40fb
myusername <183464@ids.upchiapas.edu.mx>
2020-07-20 19:43:30 -0500

    porfavor diosito

--- apiTemp/settings.py
+++ apiTemp/settings.py
@@ -59,14 +59,16 @@ MIDDLEWARE = [
 ]
 
 #CORS
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 #CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = [
     #"http://inador-console.ddns.net/",
-    "http://100.25.154.25:80",
+    #"http://100.25.154.25:80",
     #"http://localhost:8000",
-    "http://127.0.0.1:80"
+    #"http://127.0.0.1:80"
+    "http://127.0.0.1:4200",
+    "http://localhost:4200"
 ]
 
 CORS_ALLOW_METHODS = [

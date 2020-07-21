bed458b8f99554dd10383f90b34200797c3dd251
myusername <183464@ids.upchiapas.edu.mx>
2020-07-20 19:33:44 -0500

    80

--- apiTemp/settings.py
+++ apiTemp/settings.py
@@ -66,7 +66,7 @@ CORS_ORIGIN_WHITELIST = [
     #"http://inador-console.ddns.net/",
     "http://100.25.154.25:80",
     #"http://localhost:8000",
-    "http://127.0.0.1:4200:80"
+    "http://127.0.0.1:80"
 ]
 
 CORS_ALLOW_METHODS = [
